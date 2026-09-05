b7a591ac8a066c6f9a3e1a7f4547e4f3e5d3a8a2
asheshpatel65 <asheshpatel65@gmail.com>
2026-09-05 16:55:13 +0530

    second commite

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World i am learning devops!");
+        System.out.println("Hello, World i am learning devops this is my second commite !");
     }
 }
