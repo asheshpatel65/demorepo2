e29f8c08b961aca05ba8eaa77faf6cb5b76eb702
Ashesh <78936648+asheshpatel65@users.noreply.github.com>
2026-09-09 22:47:05 +0530

    Update output message in hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World i am learning devops this is my second commite !");
+        System.out.println("Hello, World i am learning devops this is my second commite and also using jenkins !");
     }
 }
